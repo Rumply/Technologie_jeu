4bd5a0b8ebe3baca32fd5d9933b6d8184a750a0f
Steve <steve.duquet@outlook.com>
2016-09-27 14:55:35 -0400

    Ajout du textToSpeech dans le MainActivity.java

--- app/src/main/java/ca/cegepdrummond/technologie_jeu/MainActivity.java
+++ app/src/main/java/ca/cegepdrummond/technologie_jeu/MainActivity.java
@@ -2,13 +2,17 @@ package ca.cegepdrummond.technologie_jeu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.MotionEvent;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
     private CanvasView customCanvas;
     private TextView mTextView;
+    private TextToSpeech tts; // Déclaration de la variable TextToSpeech
 
 
     @Override
@@ -16,12 +20,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    int result = tts.setLanguage(Locale.getDefault());
+                }
+            }
+        });
+
         customCanvas = (CanvasView) findViewById(R.id.touch_canvas);
 
 
         mTextView = (TextView) findViewById(R.id.touchState_label);
 
         customCanvas.mTextView = mTextView;
+
+        String infoToSpeak = "";
+
+        infoToSpeak = "Salut tout le monde. C'est moi la voix virtuel du jeu.";
+
+        tts.speak(infoToSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 
 
