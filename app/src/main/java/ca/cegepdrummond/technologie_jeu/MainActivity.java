9be769ac9a985d85f53af4301b174d0e366dc7c9
Rumply <rumplygnat@hotmail.com>
2016-09-27 15:38:16 -0400

    Ajout du capteur de proximité.     - Change le text si le capteur détecte un changement. Modification au touchscreen     - Ajout d'un cercle comme bouton.          - Bug on peut appuyer hors du cercle.

--- app/src/main/java/ca/cegepdrummond/technologie_jeu/MainActivity.java
+++ app/src/main/java/ca/cegepdrummond/technologie_jeu/MainActivity.java
@@ -1,15 +1,25 @@
 package ca.cegepdrummond.technologie_jeu;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
     private CanvasView customCanvas;
     private TextView mTextView;
 
+    private SensorManager mSensorManager;
+    private Sensor mProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,44 @@ public class MainActivity extends Activity {
 
         customCanvas = (CanvasView) findViewById(R.id.touch_canvas);
 
+        customCanvas.setBackground(this.getDrawable(R.drawable.shape));
+
 
         mTextView = (TextView) findViewById(R.id.touchState_label);
 
         customCanvas.mTextView = mTextView;
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (event.values[0] == 0) {
+                //Action quand un objet est proche.
+                mTextView.setText("Capteur caché");
+            } else {
+                //Action quand un objet est loin. (Couleur noir peux nuir.)
+                mTextView.setText("Capteur non caché");
+            }
+        }
+    }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
 }
