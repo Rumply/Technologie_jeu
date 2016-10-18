dbb559514d3741828ad147a40ac77ac973ea3d24
steveduquet <steve.duquet@outlook.com>
2016-10-18 13:48:48 -0400

    Ajout de la page À Propos

---
+++ app/src/main/java/ca/cegepdrummond/technologie_jeu/Propos.java
@@ -0,0 +1,15 @@
+package ca.cegepdrummond.technologie_jeu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Steve on 18 oct..
+ */
+
+public class Propos extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_jeu);
+    }
+}
